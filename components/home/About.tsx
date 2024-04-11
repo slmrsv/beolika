0950cc92f4671484bb6caf32a6973c757991226c
Selim <hello@selimmersive.com>
2024-04-11 23:52:07 +0200

    add services

--- components/home/About.tsx
+++ components/home/About.tsx
@@ -19,17 +19,18 @@ const About = () => {
         </p>
       </div>
       <div className={cn('py-6 space-y-8', 'xl:py-8 xl:space-y-0 xl:flex xl:justify-between xl:items-start')}>
-        <p className={cn("font-montserrat uppercase pt-8 text-xs", "lg:text-base lg:w-[900px]")}>Services</p>
+        <p className={cn("font-montserrat uppercase pt-8 text-xs", "lg:text-base lg:w-[900px]")}>What I do</p>
         <ul className="lg:space-y-4 w-full">
           {[
+            {title: "Web Design", description: "In my previous experiences, I was immersed in the world of design, which reinforced my interest in this field and encouraged me to deepen my skills. As a developer, I find it essential to be able to create a product by starting with the design. This approach enables me to visualize all aspects of the project before developing it, thus guaranteeing a coherent and fluid user experience."},
             {title: "Frontend Development", description: "From designs in Figma or Sketch, I create pixel-perfect responsive components, responsive web interfaces for landing pages, applications, SaaS and e-commerce, connected to back-end APIs for a seamless user experience."},
-            {title: "Backend Development", description: "I offer complete development with NextJS, including secure APIs with NextAuth, TypeScript structure and data validation with Zod. For optimum performance and security, I integrate Prisma with Zustand for real-time state management and data manipulation."},
-            {title: "3D Design", description: "With expertise in Blender modeling and a passion for architecture, I transform concepts into memorable images, enabling effective communication."},
+            {title: "Backend Development", description: "I take care of back-end development with NextJS, including authentication with NextAuth, code structure with TypeScript and data validation with Zod. For optimum performance and security, I integrate Prisma with Zustand for state management and real-time data manipulation."},
+            {title: "3D Design", description: "As a lover of architectural visualization and immersive experiences, I enjoy transforming concepts into 3D renderings with Blender in my spare time. This creative activity allows me to explore new artistic perspectives and stimulate my creativity."},
           ].map((services, index) => (
             <li key={index} className={cn('w-full text-2xl', 'xl:text-5xl')}>
               <div className={cn("border-t py-6 space-y-4 duration-500 delay-75", "xl:py-8 xl:space-y-8")}>
                 <p>{services.title}</p>
-                <p className={cn("text-base opacity-60", "xl:text-xl")}>{services.description}</p>
+                <p className={cn("text-base opacity-50", "xl:text-xl")}>{services.description}</p>
               </div>
             </li>
           ))}
