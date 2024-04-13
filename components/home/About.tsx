5de6e41d02c0f591a2937c71e417154ca6b6c94c
Selim <hello@selimmersive.com>
2024-04-14 00:55:00 +0200

    add icon for title of services

--- components/home/About.tsx
+++ components/home/About.tsx
@@ -1,5 +1,6 @@
 import { cn } from "@/lib/utils";
 import { GoArrowDownRight } from "react-icons/go";
+import { MdCircle } from "react-icons/md";
 
 const About = () => {
   return (
@@ -63,21 +64,25 @@ const About = () => {
           {[
             {
               title: "Web Design",
+              icon: <MdCircle className="text-base hidden lg:block" />,
               description:
                 "During my previous experience, I was immersed in the world of design, which reinforced my interest in this field and encouraged me to deepen my skills. As a developer, I find it essential to be able to create a product by starting with the design. This approach enables me to visualize all aspects of the project before developing it, thus guaranteeing a coherent and fluid user experience.",
             },
             {
               title: "Frontend Development",
+              icon: <MdCircle className="text-base hidden lg:block" />,
               description:
                 "From designs in Figma, I create pixel-perfect responsive components, responsive web interfaces for landing pages, web applications, SaaS and e-commerce, connected to back-end APIs for a seamless user experience.",
             },
             {
               title: "Backend Development",
+              icon: <MdCircle className="text-base hidden lg:block" />,
               description:
                 "I take care of back-end development with NextJS, including authentication with NextAuth, code structure with TypeScript and data validation with Zod. For optimum performance and security, I integrate Prisma with Zustand for state management and real-time data manipulation.",
             },
             {
               title: "3D Design",
+              icon: <MdCircle className="text-base hidden lg:block" />,
               description:
                 "As a lover of architectural visualization, I enjoy transforming concepts into 3D renderings with Blender in my spare time. This creative activity allows me to explore new artistic perspectives and stimulate my creativity.",
             },
@@ -95,11 +100,13 @@ const About = () => {
                   "xl:py-8 xl:space-y-8",
                 )}
               >
-                <p className={cn(
-                  "uppercase font-medium text-base",
-                  "lg:pt-4",
-                  "xl:text-xl",
-                )}>{services.title}</p>
+                <div className="flex gap-6 items-center">
+                  {services.icon}
+                  <p className={cn(
+                    "uppercase font-medium text-base",
+                    "xl:text-xl",
+                  )}>{services.title}</p>
+                </div>
                 <p
                   className={cn(
                     "text-sm font-light",
