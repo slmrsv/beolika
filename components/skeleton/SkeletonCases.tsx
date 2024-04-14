a785dce5942a1b5b73c8b328de88d4333d6b4da1
Selim <hello@selimmersive.com>
2024-04-14 16:23:03 +0200

    I set skeleton for cases work

--- components/skeleton/SkeletonCases.tsx
+++ components/skeleton/SkeletonCases.tsx
@@ -6,9 +6,9 @@ export default function SkeletonCases() {
     <div className={cn("h-full w-full")}>
       <Skeleton
         className={cn(
-          "h-2 w-20",
+          "h-2 w-14",
           "md:w-48",
-          "xl:w-52",
+          "xl:w-32",
         )}
       />
       <div
@@ -24,27 +24,29 @@ export default function SkeletonCases() {
         >
           <div
             className={cn(
-              "pt-20 text-left flex flex-col",
+              "pt-24 pb-14 text-left flex flex-col",
               "xl:flex-row xl:justify-between xl:items-end",
             )}
           >
-            <Skeleton className={cn("h-3 w-64")} />
-          </div>
-          <div
-            className={cn(
-              "space-y-2 pt-5 flex flex-col justify-end w-full items-end",
-            )}
-          >
-            <Skeleton className={cn("h-2 w-20")} />
-            <Skeleton className={cn("h-2 w-14")} />
-            <Skeleton className={cn("h-2 w-10")} />
-            <Skeleton className={cn("h-2 w-10")} />
-            <Skeleton className={cn("h-2 w-24")} />
+            <Skeleton className={cn("h-3 w-52", "xl:w-[1600px] xl:h-32")} />
+            <div
+              className={cn(
+                "space-y-2 pt-5 flex flex-col justify-end w-full items-end",
+              )}
+            >
+              <Skeleton className={cn("h-2 w-10")} />
+              <Skeleton className={cn("h-2 w-14")} />
+              <Skeleton className={cn("h-2 w-20")} />
+              <Skeleton className={cn("h-2 w-36")} />
+              <div className={cn("pt-5")}>
+                <Skeleton className={cn("h-2 w-20")} />
+              </div>
+            </div>
           </div>
         </div>
         <div
           className={cn(
-            "space-y-4",
+            "space-y-4 pt-6",
             "sm:space-y-6",
             "xl:flex xl:justify-between xl:gap-4 xl:space-y-0 xl:items-center",
           )}
@@ -67,6 +69,10 @@ export default function SkeletonCases() {
               "xl:h-[450px]",
             )}
           />
+        </div>
+        <div className={cn("pt-5 flex justify-between items-center")}>
+          <Skeleton className={cn("h-2 w-24")} />
+          <Skeleton className={cn("h-2 w-24")} />
         </div>
       </div>
     </div>
