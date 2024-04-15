c116f380e6407da702e994f0a02a668877fe65b3
Selim <hello@selimmersive.com>
2024-04-15 12:30:53 +0200

    add notfound page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,13 +1,26 @@
+import { cn } from "@/lib/utils";
 import Link from "next/link";
+import { GoArrowLeft } from "react-icons/go";
 
 export default async function NotFound() {
   return (
-    <div>
-      <h2>404 Not Found</h2>
-      <p>Could not find requested resource</p>
-      <p>
-        <Link href="/">back to home</Link>
-      </p>
+    <div className="h-[90vh] w-full text-center font-montserrat flex flex-col justify-center items-center">
+      <div>
+        <h2 className={cn("text-[30vw] font-medium leading-[100%]")}>4O4</h2>
+        <p className={cn("pb-10 font-light")}>The page you're looking for doesn't exist or has been moved</p>
+        <Link
+          href="/"
+          className={cn(
+            "text-xs w-full uppercase font-medium gap-2 flex justify-center items-center hover:gap-4",
+            "xl:text-2xl",
+          )}
+        >
+          <GoArrowLeft
+            className={cn("text-xs", "lg:text-lg")}
+          />
+        Back to home
+        </Link>
+      </div>
     </div>
   );
 }
